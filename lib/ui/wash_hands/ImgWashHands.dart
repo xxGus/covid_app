a9a3caae3aea78dadb48c74620434621f8713c40
gabriellab <gabrielapfvieira@gmail.com>
2020-06-06 15:58:09 -0300

    tela de como lavar as maos

---
+++ lib/ui/wash_hands/ImgWashHands.dart
@@ -0,0 +1,7 @@
+class ImgWashHands {
+  String imagem;
+
+  ImgWashHands(String imagem) {
+    this.imagem = 'assets/images/' + imagem;
+  }
+}
--- lib/ui/wash_hands/wash_hands_screen.dart
+++ lib/ui/wash_hands/wash_hands_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'ImgWashHands.dart';
 
 class WashHandsScreen extends StatefulWidget {
   @override
@@ -8,8 +9,44 @@ class WashHandsScreen extends StatefulWidget {
 class _WashHandsScreenState extends State<WashHandsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Wash Hands Screen'),
+    return MaterialApp(
+         Center(
+      child: Card(child: _body()),
+    ));
+  }
+
+  _body() {
+    List<ImgWashHands> imgwhashhands = [
+      ImgWashHands("Ativo1.png"),
+      ImgWashHands("Ativo2.png"),
+      ImgWashHands("Ativo3.png"),
+      ImgWashHands("Ativo4.png"),
+      ImgWashHands("Ativo5.png"),
+      ImgWashHands("Ativo6.png")
+    ];
+
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: imgwhashhands.length,
+      itemExtent: 300,
+      itemBuilder: (BuildContext context, int index) {
+        ImgWashHands WashHandsSteps = imgwhashhands[index];
+        return _imagem(WashHandsSteps.imagem);
+      },
     );
+  }
+
+  _imagem(String img) {
+    return Container(
+      width: 120,
+      height: 120,
+      child: Image.asset(
+        img,
+      ),
+    );
+    /*Image.asset(
+      img,
+      fit: BoxFit.cover,
+    );*/
   }
 }
